94bce76c8ae30f8162dd47a4de44bd5d4d606083
Anubhav Saini <ansaini@paypal.com>
2016-07-27 00:44:04 +0530

    Fix chainable example usage.

--- Chaining/scripts/main.js
+++ Chaining/scripts/main.js
@@ -2,6 +2,4 @@ import Chainable from './Chainable';
 
 let chainable = new Chainable();
 
-console.log(chainable.add(3));
-console.log(chainable.add(4));
-console.log(chainable.multiply(3));
+console.log(chainable.add(3).add(4).multiply(3));
