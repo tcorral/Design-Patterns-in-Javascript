dbaee20400dc8c4811a29102c8edf945a5e16152
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-07 09:44:15 +0530

    Update main.js

--- Singleton/1/scripts/main.js
+++ Singleton/1/scripts/main.js
@@ -1,6 +1,6 @@
 import Singleton from './Singleton';
 
-var oSingle1 = Singleton;
-var oSingle2 = Singleton;
+let oSingle1 = Singleton;
+let oSingle2 = Singleton;
 console.log(Singleton.toString());
 console.log("Are oSingle1 and oSingle2 the same instance? " + (oSingle1 === oSingle2));
